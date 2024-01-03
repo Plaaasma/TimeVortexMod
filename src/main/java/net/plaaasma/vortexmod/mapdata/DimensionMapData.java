1e8c9bfa1bf083a427e0a8d7f1f380813f1105a3
Plaaasma <47513553+Plaaasma@users.noreply.github.com>
2024-01-02 20:40:16 -0600

    Added saving and loading positions to the keypad

---
+++ src/main/java/net/plaaasma/vortexmod/mapdata/DimensionMapData.java
@@ -0,0 +1,49 @@
+package net.plaaasma.vortexmod.mapdata;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.saveddata.SavedData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DimensionMapData extends SavedData {
+    private static final String DATA_NAME = "saved_dim_locations";
+    private final HashMap<String, String> dataMap = new HashMap<>();
+
+    @Override
+    public CompoundTag save(CompoundTag pCompoundTag) {
+        CompoundTag dataTag = new CompoundTag();
+
+        for (Map.Entry<String, String> entry : dataMap.entrySet()) {
+            System.out.println(entry.getValue());
+            dataTag.putString(entry.getKey(), entry.getValue());
+        }
+        pCompoundTag.put(DATA_NAME, dataTag);
+
+        return pCompoundTag;
+    }
+
+    public HashMap<String, String> getDataMap() {
+        return dataMap;
+    }
+
+    public static DimensionMapData load(CompoundTag pCompoundTag) {
+        DimensionMapData savedData = new DimensionMapData();
+        CompoundTag dataTag = pCompoundTag.getCompound(DATA_NAME);
+        for (String key : dataTag.getAllKeys()) {
+            System.out.println(dataTag.getCompound(key));
+            savedData.dataMap.put(key, dataTag.getCompound(key).toString());
+        }
+        return savedData;
+    }
+
+    public static DimensionMapData get(ServerLevel world) {
+        return world.getDataStorage().computeIfAbsent(DimensionMapData::load, DimensionMapData::new, DATA_NAME);
+    }
+}
